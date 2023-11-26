43332cfd460981198032b4f1c21e641795298791
minhduc5a12 <minhduc5a15@gmail.com>
2023-11-27 00:32:54 +0700

    update trungvicuakphantu.cpp

--- Cpp/trungvicuakphantu.cpp
+++ Cpp/trungvicuakphantu.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#define duck int main
 using namespace std;
 
-void solve(vector<int>& arr, int n, int k) {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
     multiset<int> window(arr.begin(), arr.begin() + k);
-    auto mid = next(window.begin(), k / 2);
+    multiset<int>::iterator mid = next(window.begin(), k / 2);
     for (int i = k; i <= n; ++i) {
         cout << *prev(mid, 1 - (k % 2)) << ' ';
         window.insert(arr[i]);
@@ -18,19 +26,5 @@ void solve(vector<int>& arr, int n, int k) {
         }
         window.erase(window.lower_bound(arr[i - k]));
     }
-    return;
-}
-
-duck() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n, k;
-    cin >> n >> k;
-    vector<int> arr(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    solve(arr, n, k);
     return 0;
 }
