0054511125a1e91791a4407b852d1535995d366e
minhduc5a12 <minhduc5a15@gmail.com>
2023-11-26 18:27:52 +0700

    add files

---
+++ Cpp/phantichsothanhtongmotdaykhonggiamcacsonguyenduong.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solve(int n) {
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = i; j <= n; ++j) {
+            dp[j] += dp[j - i];
+        }
+    }
+    return dp[n];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << solve(n) << endl;
+    }
+    return 0;
+}
