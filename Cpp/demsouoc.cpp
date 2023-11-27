2395a29be493315b08255b2f6df2a0c203ba8287
minhduc5a12 <minhduc5a15@gmail.com>
2023-11-27 10:12:16 +0700

    add new files

---
+++ Cpp/demsouoc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long solve(long long n) {
+    return n / 3 + n / 5 + n / 7 - n / 15 - n / 21 - n / 35 + n / 105;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        long long n;
+        cin >> n;
+        cout << solve(n) << endl;
+    }
+    return 0;
+}
