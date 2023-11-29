cd56f1ff14037675d5514abc5447dbacff6e3ea8
minhduc5a12 <minhduc5a15@gmail.com>
2023-11-29 22:35:32 +0700

    create 5.chiateam.cpp

---
+++ Cpp/5.chiateam.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+#define ll long long
+using namespace std;
+
+map<char, ll> dict = { {'M', 0}, {'A', 0}, {'R', 0}, {'C', 0}, {'H', 0} };
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        string s;
+        cin >> s;
+        if (s[0] == 'M' || s[0] == 'A' || s[0] == 'R' || s[0] == 'C' || s[0] == 'H') {
+            ++dict[s[0]];
+        }
+    }
+    ll a = dict['M'], b = dict['A'], c = dict['R'], d = dict['C'], e = dict['H'];
+    cout << a * b * (c + d + e) + c * (a + b) * (d + e) + d * e * (a + b + c);
+    return 0;
+}
