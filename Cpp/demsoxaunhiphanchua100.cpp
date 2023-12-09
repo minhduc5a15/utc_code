24fb350441a9bb88ed5040850de10c8e038c8ea9
minhduc5a12 <minhduc5a15@gmail.com>
2023-12-10 04:05:45 +0700

    update demsoxaunhiphanchua100.cpp

--- Cpp/demsoxaunhiphanchua100.cpp
+++ Cpp/demsoxaunhiphanchua100.cpp
@@ -9,46 +9,61 @@ using std::ios_base;
 using std::vector;
 using vectorll = vector<ll>;
 using matrixll = vector<vector<ll>>;
-matrixll multiply(const matrixll &matrixA, const matrixll &matrixB) {
-    matrixll result(SIZE, vectorll(SIZE, 0));
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            for (int k = 0; k < SIZE; ++k) {
-                result[i][j] = (result[i][j] + matrixA[i][k] * matrixB[k][j]) % MOD;
+
+class Fibo {
+private:
+    ll n;
+    matrixll F = {{1, 1}, {1, 0}};
+public:
+    Fibo(ll n) {
+        this->n = n;
+    }
+    matrixll multiply(const matrixll &matrixA, const matrixll &matrixB) {
+        matrixll result(SIZE, std::vector<ll>(SIZE, 0));
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                for (int k = 0; k < SIZE; ++k) {
+                    result[i][j] = (result[i][j] + matrixA[i][k] * matrixB[k][j]) % MOD;
+                }
             }
         }
+        return result;
     }
-    return result;
-}
+    matrixll power(const matrixll &base = {{1, 1}, {1, 0}}) {
+        matrixll result = {{1, 0}, {0, 1}};
+        matrixll current_base = base;
+        ll exponent = this->n + 1;
+        while (exponent) {
+            if (exponent & 1) {
+                result = multiply(result, current_base);
+            }
+            current_base = multiply(current_base, current_base);
+            exponent >>= 1;
+        }
+        return result;
+    }
 
-matrixll power(matrixll base, ll exponent) {
-    matrixll result = {{1, 1}, {1, 0}};
-    while (exponent) {
-        if (exponent & 1) {
-            result = multiply(result, base);
+    ll power(ll base) {
+        base %= MOD;
+        ll result = 1;
+        while (n) {
+            if (n & 1) {
+                result = (result * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            n >>= 1;
         }
-        base = multiply(base, base);
-        exponent >>= 1;
+        return result;
     }
-    return result;
-}
-ll power(ll base, ll exponent) {
-    base %= MOD;
-    ll result = 1;
-    while (exponent) {
-        if (exponent & 1) {
-            result = (result * base) % MOD;
-        }
-        base = (base * base) % MOD;
-        exponent >>= 1;
+
+    ll fib() {
+        F = this->power(F);
+        return F[0][0] % MOD;
     }
-    return result;
-}
-ll fib(ll n) {
-    matrixll F = {{1, 1}, {1, 0}};
-    power(F, n - 1);
-    return F[0][0] % MOD;
-}
+    ll result() {
+        return (this->power(2) - this->fib() + 1 + MOD) % MOD;
+    }
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -59,7 +74,10 @@ int main() {
     while (t--) {
         ll n;
         cin >> n;
-        cout << (power(2, n) - fib(n + 3) + 1 + MOD) % MOD << '\n';
+        Fibo *fibo = new Fibo(n);
+        cout << fibo->result() << '\n';
+        delete fibo;
     }
+
     return 0;
 }
