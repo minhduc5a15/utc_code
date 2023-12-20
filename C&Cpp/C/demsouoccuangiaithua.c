f2af5198f5f7e316591b5d79a273a820276e2d2b
Pham Duc <minhduc5a15@gmail.com>
2023-12-21 02:52:15 +0700

    update

--- C&Cpp/C/demsouoccuangiaithua.c
+++ C&Cpp/C/demsouoccuangiaithua.c
@@ -14,32 +14,26 @@ int total_pow(int n, int p) {
     return res;
 }
 
-bool* Sieve_of_Eratosthenes(int n, int* primeCount) {
-    bool* prime = (bool*)malloc((n + 1) * sizeof(bool));
-    for (int i = 0; i <= n; ++i) prime[i] = true;
-    prime[0] = prime[1] = false;
+bool *Sieve(int n) {
+    bool *result = (bool *) malloc((n + 1) * sizeof(bool));
+    for (int i = 0; i <= n; ++i) result[i] = true;
+    result[0] = result[1] = false;
     int p = 2;
     while (p * p <= n) {
-        if (prime[p]) {
+        if (result[p]) {
             for (int i = p * p; i <= n; i += p) {
-                prime[i] = false;
+                result[i] = false;
             }
         }
         ++p;
     }
-    *primeCount = 0;
-    for (p = 2; p <= n; ++p) {
-        if (prime[p]) {
-            (*primeCount)++;
-        }
-    }
-    return prime;
+
+    return result;
 }
 
 ll solve(int n) {
     ll res = 1;
-    int primeCount;
-    bool* primes = Sieve_of_Eratosthenes(n, &primeCount);
+    bool *primes = Sieve(n);
     for (int i = 2; i <= n; ++i) {
         if (primes[i]) {
             res = (res * total_pow(n, i)) % MOD;
