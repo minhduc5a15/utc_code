a95954c3f5be1c9409171107682d3eb377596fc3
Pham Duc <minhduc5a15@gmail.com>
2023-12-24 11:41:14 +0700

    update

--- C&Cpp/C/main.c
+++ C&Cpp/C/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main() {
-    int *arr = (int *) malloc(10 * sizeof(int));
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &arr[i]);
+    int n;
+    scanf("%d", &n);
+    while (n--) {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        if (a > b) swap(&a, &b);
+        printf("%s\n", (b <= a * (c + 1)) ? "YES" : "NO");
     }
-    for (int i = 0; i < 10; i++) {
-        printf("%d\n", arr[i]);
-    }
-    free(arr);
     return 0;
 }
