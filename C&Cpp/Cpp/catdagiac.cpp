5247d128df512bb5e74a1566d2003a341809c1aa
Pham Duc <minhduc5a15@gmail.com>
2024-01-04 20:12:13 +0700

    update

---
+++ C&Cpp/Cpp/catdagiac.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, k;
+    cin >> n >> k;
+    while (k--) {
+        int p;
+        cin >> p;
+        n -= p - 2;
+    }
+    if (n == 2) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
