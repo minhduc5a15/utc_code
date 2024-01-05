f6e7ee4771a8b1e86bbf597cbe9b1073c3709815
Pham Duc <104719252+minhduc5a15@users.noreply.github.com>
2024-01-06 00:09:15 +0700

    Create chaodonsinhvienk59.cpp

---
+++ C&Cpp/Cpp/chaodonsinhvienk59.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    vector<int> right(n, -1);
+    vector<int> left(n, -1);
+    stack<int> s;
+    for (int i = 0; i < n; ++i) {
+        while (!s.empty() && arr[s.top()] < arr[i]) {
+            right[s.top()] = i;
+            s.pop();
+        }
+        s.push(i);
+    }
+    while (!s.empty()) s.pop();
+    for (int i = n - 1; i >= 0; --i) {
+        while (!s.empty() && arr[s.top()] < arr[i]) {
+            left[s.top()] = i;
+            s.pop();
+        }
+        s.push(i);
+    }
+    for (int i = 0; i < n; ++i) {
+        if (right[i] == -1 && left[i] == -1) cout << -1 << ' ';
+        else if (right[i] != -1 && left[i] == -1) cout << right[i] << ' ';
+        else if (right[i] == -1 && left[i] != -1) cout << left[i] << ' ';
+        else {
+            if (right[i] - i < i - left[i]) cout << right[i] << ' ';
+            else cout << left[i] << ' ';
+        }
+    }
+    return 0;
+}
