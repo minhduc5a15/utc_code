54659904f467b5ae535973371fe013d75d144748
Pham Duc <minhduc5a15@gmail.com>
2024-02-23 01:01:09 +0700

    update

--- Cpp/0.sohoanhao.cpp
+++ Cpp/0.sohoanhao.cpp
@@ -7,13 +7,11 @@ int main() {
     cout.tie(nullptr);
     int n;
     cin >> n;
-    for (int i = 0; i < n; ++i) {
+    while (n--) {
         long long x;
         cin >> x;
-        if (x == 6 || x == 28 || x == 496 || x == 8128 || x == 33550336 || x == 8589869056 || x == 137438691328)
-            cout << x << " eh perfeito\n";
-        else
-            cout << x << " nao eh perfeito\n";
+        if (x == 6 || x == 28 || x == 496 || x == 8128 || x == 33550336 || x == 8589869056 || x == 137438691328) cout << x << " eh perfeito\n";
+        else cout << x << " nao eh perfeito\n";
     }
     return 0;
 }
