d4b5607e8f6e9b8a58e6972494913b19a2b4f0b8
Pham Duc <minhduc5a15@gmail.com>
2024-03-25 11:41:20 +0700

    update

--- Rust/src/sinhcacdaynhiphankhongchua101.rs
+++ Rust/src/sinhcacdaynhiphankhongchua101.rs
@@ -3,9 +3,9 @@ fn solve(s: &str, n: i32) {
     if sz == n as usize {
         return println!("{}", s);
     }
-    solve(s + '0', n);
+    solve(&(s.to_string() + "0"), n);
     if sz < 2 || (&s[sz - 2..sz] != "10") {
-        solve(s + '1', n);
+        solve(&(s.to_string() + "1"), n);
     }
 }
 
@@ -13,6 +13,5 @@ fn main() {
     let mut n: String = String::new();
     std::io::stdin().read_line(&mut n).unwrap();
     let n: i32 = n.trim().parse().unwrap();
-    solve("0", n);
-    solve("1", n);
+    solve("", n);
 }
