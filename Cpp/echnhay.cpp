4446d3af35357fcb0e569d15ef1abcf2b650dbdf
Pham Duc <minhduc5a15@gmail.com>
2024-04-16 07:58:31 +0700

    ✔✔✔

--- Cpp/echnhay.cpp
+++ Cpp/echnhay.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-bool solve(const vector<int>& arr, int n) {
+bool solve(const vector<int> &arr, int n) {
     int max_reach = arr.front();
     for (int i = 0; i < n; ++i) {
         if (i > max_reach) return false;
@@ -10,21 +10,24 @@ bool solve(const vector<int>& arr, int n) {
     }
     return true;
 }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     int t;
     cin >> t;
+    vector<int> arr;
     while (t--) {
         int n;
         cin >> n;
-        vector<int> arr(n);
+        arr.resize(n);
         for (int i = 0; i < n; ++i) {
             cin >> arr[i];
         }
-        if (solve(arr, n)) cout << "True" << endl;
-        else cout << "False" << endl;
+        if (solve(arr, n)) cout << "True\n";
+        else cout << "False\n";
+        arr.clear();
     }
     return 0;
 }
