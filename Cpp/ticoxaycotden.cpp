638b0f3f3db4a7c33a0a0620cd6714c370af4473
Pham Duc <minhduc5a15@gmail.com>
2024-06-23 05:01:48 +0700

    comeback

---
+++ Cpp/ticoxaycotden.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+ll calc(ll n) {
+    ll res = 0;
+    for (ll i = 2; i <= n; i <<= 1) res += n / i;
+    return res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll l, r;
+    cin >> l >> r;
+    cout << calc(r) - calc(l - 1);
+    return 0;
+}
