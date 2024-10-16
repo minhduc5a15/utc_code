7bd85c41579238e7ed014ea1a8882ffa31df8694
minhduc5a15 <minhduc5a15@gmail.com>
2024-10-16 21:03:13 +0700

    ✔✔✔

--- Rust/src/bocchinacci.rs
+++ Rust/src/bocchinacci.rs
@@ -1,12 +1,12 @@
 use std::io::{stdin, BufRead};
 
-const MOD: u128 = 1000000000000007;
+const MOD: u64 = 1000000007;
 const SIZE: usize = 3;
 
-type Matrix = [[u128; SIZE]; SIZE];
+type Matrix = [[u64; SIZE]; SIZE];
 
 fn multiply(matrix_a: &Matrix, matrix_b: &Matrix) -> Matrix {
-    let mut result = [[0u128; SIZE]; SIZE];
+    let mut result = [[0u64; SIZE]; SIZE];
     for i in 0..SIZE {
         for j in 0..SIZE {
             for k in 0..SIZE {
@@ -17,8 +17,8 @@ fn multiply(matrix_a: &Matrix, matrix_b: &Matrix) -> Matrix {
     result
 }
 
-fn power(mut base: Matrix, mut exponent: u128) -> Matrix {
-    let mut result = [[1u128, 0, 0], [0, 1, 0], [0, 0, 1]];
+fn power(mut base: Matrix, mut exponent: u64) -> Matrix {
+    let mut result = [[1u64, 0, 0], [0, 1, 0], [0, 0, 1]];
     while exponent > 0 {
         if exponent & 1 == 1 {
             result = multiply(&result, &base);
@@ -29,24 +29,20 @@ fn power(mut base: Matrix, mut exponent: u128) -> Matrix {
     result
 }
 
-fn solve(n: u128) -> u128 {
+fn solve(n: u64) -> u64 {
     let f: Matrix = [[1, 1, 1], [1, 0, 0], [0, 1, 0]];
-    let v: [u128; 3] = [1, 1, 0];
+    let v: [u64; 3] = [1, 1, 0];
     let f_n = power(f, n);
     let mut res = 0;
     for i in 0..SIZE {
         res = (res + (f_n[0][i] * v[i]) % MOD) % MOD;
     }
-    res - 1
+    res
 }
 
 fn main() {
     let mut lines = stdin().lock().lines();
     let line = lines.next().unwrap().unwrap();
-    let t: usize = line.parse().unwrap();
-    for _ in 0..t {
-        let line = lines.next().unwrap().unwrap();
-        let n: u128 = line.parse().unwrap();
-        println!("{}", solve(n));
-    }
+    let n: u64 = line.trim().parse().unwrap();
+    println!("{}", solve(n));
 }
